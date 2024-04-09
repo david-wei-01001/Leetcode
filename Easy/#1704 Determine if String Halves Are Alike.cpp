bbe9bc83184d4609017248d8daec95522df50f3e
Jiankun Wei <72998341+david-wei-01001@users.noreply.github.com>
2024-04-09 11:46:50 -0400

    Update #1704 Determine if String Halves Are Alike.cpp

--- Easy/#1704 Determine if String Halves Are Alike.cpp
+++ Easy/#1704 Determine if String Halves Are Alike.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int isVowel(const char chr){
-        char vowellst[] = "aeiouAEIOU";
-        for (int i = 0; i < strlen(vowellst); i++){
-            if (chr == vowellst[i]){
-                return 1;
-            }
-        }
-        return 0;
-    }
     bool halvesAreAlike(string s) {
+        unordered_set<char> vowels = {
+        'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+        };
         int len = s.length() / 2;
         int a_count = 0;
         int b_count = 0;
         for (int i = 0; i < len; i++){
-            a_count += isVowel(s[i]);
-            b_count += isVowel(s[i + len]);
+            a_count += vowels.contains(s[i]);
+            b_count += vowels.contains(s[i + len]);
         }
         return a_count == b_count;
     }
